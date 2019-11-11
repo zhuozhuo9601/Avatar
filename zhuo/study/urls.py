2d62011106b5dadebf7ea9a5ee3d30c01f6ed101
fanlizhuo <zhuozhuo9601@mail.com>
2019-11-11 16:28:59 +0800

    邮件发送功能

--- zhuo/study/urls.py
+++ zhuo/study/urls.py
@@ -4,5 +4,6 @@ from study import views
 
 urlpatterns = [
     url(r'^study_first/$', views.study_first,name='study_first'),
+    url(r'^study_send/$', views.send,name='send'),
     # url(r'^study_first/(?P<id>\d+)/$', views.study_first,name='study_first'),
 ]
--- zhuo/study/views.py
+++ zhuo/study/views.py
@@ -1,7 +1,50 @@
+import json
+import smtplib
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from django import http
+
 from django.shortcuts import render
 
 # Create your views here.
+from text.base import user_login
 
 
+@user_login
 def study_first(request):
     return render(request, 'study.html')
+
+
+# 发送邮件
+@user_login
+def send(request):
+    # 发件人
+    sender = request.POST.get('username')
+    # 接收者
+    receiver = request.POST.get('receiver')
+    # 邮件服务器
+    smtpserver = 'smtp.163.com'
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    theme = request.POST.get('theme')
+    mail_title = theme
+
+    # 创建一个带附件的实例
+    message = MIMEMultipart()
+    message['From'] = sender
+    message['To'] = receiver
+    message['Subject'] = Header(mail_title, 'utf-8')
+
+    # 邮件正文内容
+    content = request.POST.get('content')
+    message.attach(MIMEText(content, 'plain', 'utf-8'))
+
+    smtpObj = smtplib.SMTP()  # 注意：如果遇到发送失败的情况（提示远程主机拒接连接），这里要使用SMTP_SSL方法
+    smtpObj.connect(smtpserver)
+    smtpObj.login(username, password)
+    smtpObj.sendmail(sender, receiver, message.as_string())
+    print("邮件发送成功！！！")
+    smtpObj.quit()
+    return http.HttpResponse('200')
